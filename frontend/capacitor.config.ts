cc867953e9665433148d829d136c9543d7597544
stella0024 <vincenzostella00@gmail.com>
2026-03-25 20:27:36 +0100

    apk version 1

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.workshift.app',
+  appName: 'Workshift',
+  webDir: 'dist'
+};
+
+export default config;
